9c2b1ee966d53cf5ac339bfe998ce609b01f8df7
fahmiirawan0803 <117510609+fahmiirawan0803@users.noreply.github.com>
2022-11-09 15:19:56 +0700

    halo

--- src/JawabanNomerTiga.java
+++ src/JawabanNomerTiga.java
@@ -1,5 +1,7 @@
 public class JawabanNomerTiga {
     public static void main(String[] args) {
         System.out.println("Hello Kelas 1D");
+        System.out.println("hallo dek");
+        System.out.println("hallo gaes");
     }
 }
